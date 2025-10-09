d65ccbb5173aa017fd5ef2c4c0b8faddf1a89500
MAC <mac@MACs-Mac-mini.local>
2025-10-09 16:10:15 +0800

    Fix BrowserRouter with explicit route rewrites

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import MessageScore from './MessageScore.jsx'
 import About from './About.jsx'
@@ -9,13 +9,13 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<MessageScore />} />
         <Route path="/about" element={<About />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
       </Routes>
       <Analytics />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
