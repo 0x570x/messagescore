9579adf17ca40c835109f96af66167abbe23babf
MAC <mac@MACs-Mac-mini.local>
2025-10-09 15:09:35 +0800

    Switch to HashRouter temporarily

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import MessageScore from './MessageScore.jsx'
 import About from './About.jsx'
@@ -9,13 +9,13 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MessageScore />} />
         <Route path="/about" element={<About />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
       </Routes>
       <Analytics />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
